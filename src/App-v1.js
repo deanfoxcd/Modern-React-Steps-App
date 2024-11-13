fe09a23004fe8fe75be4c8f22e3b153cbe6f177c
Dean Fox <deanfoxcd@gmail.com>
2024-11-12 21:41:04 -0500

    add reusable buttons in new file, copy of App.js

--- src/App-v1.js
+++ src/App-v1.js
@@ -43,32 +43,21 @@ export default function App() {
           </p>
 
           <div className="buttons">
-            <Button
-              bgColor="#7950f2"
-              textColor="#fff"
-              text="Prev"
+            <button
+              style={{ backgroundColor: '#7950f2', color: '#fff' }}
               onClick={handlePrev}
-            />
-            <Button
-              bgColor="#7950f2"
-              textColor="#fff"
-              text="Next"
+            >
+              Previous
+            </button>
+            <button
+              style={{ backgroundColor: '#7950f2', color: '#fff' }}
               onClick={handleNext}
-            />
+            >
+              Next
+            </button>
           </div>
         </div>
       )}
     </>
   );
 }
-
-const Button = ({ textColor, bgColor, onClick, text }) => {
-  return (
-    <button
-      style={{ backgroundColor: bgColor, color: textColor }}
-      onClick={onClick}
-    >
-      {text}
-    </button>
-  );
-};
